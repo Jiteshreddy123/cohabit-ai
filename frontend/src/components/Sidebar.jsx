d17808660adfce11804b081fe0ccb5137102b145
Sai_Surya <nampallysaisurya@gmail.com>
2026-07-01 01:59:07 +0530

    Create dashboard structure

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+function Sidebar() {
+
+    return (
+
+        <div style={{
+            width: "220px",
+            background: "#f5f5f5",
+            padding: "20px",
+            minHeight: "90vh"
+        }}>
+
+            <h3>Menu</h3>
+
+            <p>Dashboard</p>
+
+            <p>Students</p>
+
+            <p>Interview</p>
+
+            <p>Recommendations</p>
+
+        </div>
+
+    );
+}
+
+export default Sidebar;
